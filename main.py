0cc7733fe3dd74c416444b64110c1a254bede15d
Doppie22 <dvopstal05@gmail.com>
2022-09-11 16:51:34 +0200

    fix niet in kanaal bericht nneit werken

--- main.py
+++ main.py
@@ -45,13 +45,14 @@ async def self(interaction: discord.Interaction):
 
 @tree.command(name="outro", description="outro", guild=guild)
 async def self(interaction: discord.Interaction):
-    voice_channel = interaction.user.voice.channel
+    voice_channel = interaction.user.voice
     channel = None
     if voice_channel != None:
+        voice_channel = voice_channel.channel
         channel = voice_channel.name
         incall = voice_channel.members
         vc = await voice_channel.connect()
-        await interaction.response.send_message("speelt nu outro")
+        await interaction.response.send_message("SMASH THAT LIKE BUTTON :thumbsup:")
         vc.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source="D:\.onedrive bestanden\creatief\Code\Dsicord bot\wiebot\outro\outro kort.wav"))
         while vc.is_playing():
             await sleep(0.1)
@@ -59,17 +60,18 @@ async def self(interaction: discord.Interaction):
         for member in incall:
             await member.move_to(None)
     else:
-        await interaction.response.send_message(str(interaction.user.name), "ziet niet in een spraakkanaal")
+        await interaction.response.send_message("Join eerst een spraakkanaal.")
 
 @tree.command(name="crabrave", description="Epic traditie", guild=guild)
 async def self(interaction: discord.Interaction):
-    voice_channel = interaction.user.voice.channel
+    voice_channel = interaction.user.voice
     channel = None
     if voice_channel != None:
+        voice_channel = voice_channel.channel
         channel = voice_channel.name
         incall = voice_channel.members
         vc = await voice_channel.connect()
-        await interaction.response.send_message("speelt nu Crab Rave")
+        await interaction.response.send_message(":crab: ")
         vc.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source="D:\.onedrive bestanden\creatief\Code\Dsicord bot\wiebot\outro\crab rave kort.wav"))
         while vc.is_playing():
             await sleep(0.1)
@@ -77,7 +79,7 @@ async def self(interaction: discord.Interaction):
         for member in incall:
             await member.move_to(None)
     else:
-        await interaction.response.send_message(str(interaction.user.name), "ziet niet in een spraakkanaal")
+        await interaction.response.send_message("Join eerst een spraakkanaal.")
 
 @client.event
 async def on_message(message):
